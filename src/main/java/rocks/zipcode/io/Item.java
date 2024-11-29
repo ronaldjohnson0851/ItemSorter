8b53424e81abc42f9bd63c41337976ab50ddddde
rjohnson0851 <ron21johnson@comcast.net>
2024-11-29 15:50:18 -0500

    Finished item

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,47 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+    private Long idNum;
+    private String name;
+    private Double price;
+
     public Item() {
+        this.idNum = 0L;
+        this.name = "";
+        this.price = 0.01;
     }
 
     public Item(Long id, String name, Double price) {
+        idNum = id;
+        this.name = name;
+        this.price = price;
     }
 
     public String getName() {
-        return null;
+
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Long getId() {
-        return null;
+
+        return idNum;
     }
 
     public void setId(Long id) {
+        idNum = id;
     }
 
     public Double getPrice() {
-        return null;
+
+        return price;
     }
 
     public void setPrice(Double price) {
+        this.price = price;
+
     }
 }
